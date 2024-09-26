64199ab98d956004f2f8e3b3209ac330fd665366
Istiak Ahamed <istiak.borsho@gmail.com>
2024-09-26 09:07:37 +0600

    Adding Computed Column

--- store/admin.py
+++ store/admin.py
@@ -3,11 +3,16 @@ from . import models
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price']
+    list_display = ['title', 'unit_price', 'inventory_status']
     list_editable = ['unit_price']
     list_per_page = 10
     ordering = ['title']
-
+    @admin.display(ordering='inventory')
+    def inventory_status(self, product):
+        if product.inventory < 10:
+            return 'Low'
+        else:
+            return 'ok'
 admin.site.register(models.Collection)
 
 @admin.register(models.Customer)
