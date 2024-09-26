c939fcd37b2e72774dbb712cdecdceacace94d46
Istiak Ahamed <istiak.borsho@gmail.com>
2024-09-26 09:26:14 +0600

    Adding Collection Admin

--- store/admin.py
+++ store/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from django.db.models.aggregates import Count
+from django.db.models.query import QuerySet
+from django.http import HttpRequest
 from . import models
 
 @admin.register(models.Product)
@@ -19,7 +22,21 @@ class ProductAdmin(admin.ModelAdmin):
             return 'Low'
         else:
             return 'ok'
-admin.site.register(models.Collection)
+
+@admin.register(models.Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = [
+        "title",
+        "products_count"
+    ]
+    @admin.display(ordering='products_count')
+    def products_count(self, collection):
+        return collection.products_count
+    
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(
+            products_count = Count('product')
+        )
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
