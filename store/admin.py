23152b0257bede126c9a17ad36f5a808cc2c76fa
Istiak Ahamed <istiak.borsho@gmail.com>
2024-09-26 09:17:42 +0600

    Added Customer Page

--- store/admin.py
+++ store/admin.py
@@ -3,10 +3,16 @@ from . import models
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price', 'inventory_status']
+    list_display = ['title', 'unit_price', 'inventory_status', 'collection_title']
     list_editable = ['unit_price']
     list_per_page = 10
+    list_select_related = ['collection']
     ordering = ['title']
+
+    def collection_title(self, product):
+        return product.collection.title
+
+
     @admin.display(ordering='inventory')
     def inventory_status(self, product):
         if product.inventory < 10:
@@ -21,3 +27,8 @@ class CustomerAdmin(admin.ModelAdmin):
     list_editable = ['membership']
     ordering = ['first_name', 'last_name']
     list_per_page = 10
+
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'placed_at', 'customer']
